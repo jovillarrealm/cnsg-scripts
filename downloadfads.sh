f16a8d775025b176b43f7db63ad51003e60db40e
jovillarrealm <javillarrm@eafit.edu.co>
2024-08-09 10:29:09 -0500

    conda installer

--- downloadfads.sh
+++ downloadfads.sh
@@ -1,3 +1,4 @@
 wget https://github.com/ParBLiSS/FastANI/releases/download/v1.34/fastANI-linux64-v1.34.zip
 wget https://ftp.ncbi.nlm.nih.gov/pub/datasets/command-line/v2/linux-amd64/datasets
-wget https://ftp.ncbi.nlm.nih.gov/pub/datasets/command-line/v2/linux-amd64/dataformat
+wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
+
