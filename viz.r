2902cf1f9c3061c71e01a554daaf0d1bb31c20ac
jovillarrealm <javillarrm@eafit.edu.co>
2024-08-28 12:59:20 -0500

    swap memory used, kepping it 1000

--- viz.r
+++ viz.r
@@ -1,27 +1,32 @@
-library(tidyverse, help, pos = 2, lib.loc = NULL)
-# library(lintr, help, pos = 3, lib.loc = NULL)
-# library(httpgd, help, pos = 4, lib.loc = NULL)
-# library(languageserver, help, pos = 5, lib.loc = NULL)
-library(ggplot2, help, pos = 6, lib.loc = NULL)
+library(tidyverse)
+
+# Load the data
 recs <- read.csv(
   file = "/home/jorge/22julia/eDNA-empty/ptester/recs.csv",
   header = TRUE,
   sep = ";"
 )
-print(recs)
-recs <- recs[order(recs$comparisons_number), ]
-print(recs)
-# scale_x_log10() +
-ggplot(recs, aes(x = comparisons, y = user_time)) +
-  scale_x_discrete() +
-  scale_y_log10() +
+
+# Sort by comparisons_number and group by comparisons
+recs_sorted_grouped <- recs %>%
+  group_by(comparisons) %>%
+  summarize(
+    avg_user_time = mean(user_time, na.rm = TRUE),
+    total_comparisons_number = sum(comparisons_number, na.rm = TRUE),
+  ) %>%
+  arrange(total_comparisons_number) %>%
+  mutate(comparisons = factor(comparisons, levels = comparisons)) # Ensure the order in the plot
+
+# Plot the grouped and sorted data
+ggplot(recs_sorted_grouped, aes(x = comparisons, y = avg_user_time)) +
   geom_bar(stat = "identity") +
+  scale_y_log10() +
   labs(
-    title = "Scatter Plot of log(Comparisons) vs log(User Time)",
+    title = "Plot of log(User Time (s)) by Comparisons",
     x = "Comparisons (QxR)",
-    y = "User Time (s)"
+    y = "log(Average User Time) (s)"
   ) +
-  theme_minimal()
+  theme_classic()
 
 # Print the plot explicitly (optional)
 print(last_plot())
